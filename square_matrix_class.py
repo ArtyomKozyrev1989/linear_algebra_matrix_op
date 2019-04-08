db2da4a2ce3d10ed978b95ea5847238f1e93a871
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-04-08 13:19:50 +0300

    Update square_matrix_class.py

--- square_matrix_class.py
+++ square_matrix_class.py
@@ -15,6 +15,18 @@ class SquareMatrix:
                     self.determinant = SquareMatrix.matrix_determinant(self)
                     
                     
+    def __str__(self):
+        full_matrix = ""
+        for i in range(len(self)):
+            line = "  {:^10}  " * len(self)
+            full_matrix += line.format(*self.matrix[i]) + "\n\n"
+        return full_matrix
+        
+                                
+    def __repr__(self):
+        return "SquareMatrix({})".format(self.matrix)
+    
+                    
     def _get_smaller_matrix(matrix, row_number):
         new_matrix = []
         for line_number in range(1, len(matrix)):
@@ -27,9 +39,9 @@ class SquareMatrix:
     
     
     def matrix_determinant(self):
-        if len(self.matrix) == 1:
+        if len(self) == 1:
             return self.matrix[0][0]
-        elif len(self.matrix) == 2:
+        elif len(self) == 2:
             return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
         else:
             det_value = 0
@@ -91,6 +103,44 @@ class SquareMatrix:
                     new_line.append(another * self.matrix[line_index][item_index])
                 new_matrix.append(new_line)
             return SquareMatrix(new_matrix)
+        elif type(another) == SquareMatrix:
+            if len(self) != len(another):
+                raise ValueError("The matrixes should have the same deimensions")
+            else:
+                another_tr = another.transposition()
+                new_matrix = []
+                for i in range(len(self)):
+                    new_line = []
+                    for j in range(len(self)):
+                        el_sum = 0
+                        for z in range(len(self)):
+                            el_sum += self.matrix[i][z] * another_tr.matrix[j][z]
+                        new_line.append(el_sum)
+                    new_matrix.append(new_line)
+                return SquareMatrix(new_matrix)     
         else:
-            raise ValueError("XXX")
+            raise ValueError("SquareMatrix can be multiplied only by integer, float or SquareMatrix")
             
+            
+    def __rmul__(self, another):
+        if type(another) == int or type(another) == float:
+            new_matrix = []
+            for line_index in range(len(self)):
+                new_line = []
+                for item_index in range(len(self)):
+                    new_line.append(another * self.matrix[line_index][item_index])
+                new_matrix.append(new_line)
+            return SquareMatrix(new_matrix)
+        else:
+            raise ValueError("SquareMatrix can be multiplied only by integer, float or SquareMatrix")
+            
+    
+    def transposition(self):
+        new_matrix = []
+        k = 0 
+        while k < len(self.matrix):
+            new_matrix.append([])
+            for line in self.matrix:
+                new_matrix[k].append(line[k])
+            k+=1
+        return SquareMatrix(new_matrix)
